6398db2e77a41ec3503ca837b7ae560ddbfac474
Lautaro Amado <lautaa256@gmail.com>
2025-06-05 00:32:03 -0300

    Aca van todos los archivos que se crearon y cree para utilizar react y sus componentes

---
+++ react/libreria-react/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+// src/components/Footer.jsx
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="lightblue" id="pie">
+      <br />
+      <p>&copy; 2025 Librería. Todos los derechos reservados.</p>
+      <div id="redes-sociales">
+        <a href="#">Facebook</a><br />
+        <a href="#">Twitter</a><br />
+        <a href="#">Instagram</a>
+      </div>
+      <p><a href="#">Términos y condiciones</a></p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ react/libreria-react/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div id="cabecera">
+      <img src="../../Ejercicio5/imagenes/logotipo-libreria-dibujado-mano_23-2149331221.avif" alt="Logo de la Librería" id="logo" />
+      <h1>Nombre Librería</h1>
+    </div>
+  );
+}
+
+export default Header;
